000b4e32b464010db97c45038a515497d6695fa7
Alexa <>
2018-05-24 23:56:26 +0000

    ASK SDK v2 for Java 2.3.0

--- ask-sdk-core/src/com/amazon/ask/util/SdkConstants.java
+++ ask-sdk-core/src/com/amazon/ask/util/SdkConstants.java
@@ -23,6 +23,6 @@ public final class SdkConstants {
     /**
      * The version number for the SDK.
      */
-    public static final String SDK_VERSION = "2.2.1";
+    public static final String SDK_VERSION = "2.3.0";
 
 }
