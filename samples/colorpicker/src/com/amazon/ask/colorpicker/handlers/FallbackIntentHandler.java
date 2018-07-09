0f584d1682c84b2db7ea1bdcbd70bf24dd4f809f
Olivia Sung <sungyoo@amazon.com>
2018-07-09 15:54:52 -0700

    Adding a comment for FallbackIntent is only available in en-US region

--- samples/colorpicker/src/com/amazon/ask/colorpicker/handlers/FallbackIntentHandler.java
+++ samples/colorpicker/src/com/amazon/ask/colorpicker/handlers/FallbackIntentHandler.java
@@ -8,6 +8,9 @@ import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
 
+// 2018-July-09: AMAZON.FallackIntent is only currently available in en-US locale.
+//              This handler will not be triggered except in that locale, so it can be
+//              safely deployed for any locale.
 public class FallbackIntentHandler implements RequestHandler {
 
     @Override
