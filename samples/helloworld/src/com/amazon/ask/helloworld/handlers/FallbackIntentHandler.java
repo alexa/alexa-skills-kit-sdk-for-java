1217c62c7b3f92e2693aa34d2192869f9cadd0ce
Olivia Sung <sungyoo@amazon.com>
2018-07-09 15:50:11 -0700

    Create FallbackIntentHandler.java

---
+++ samples/helloworld/src/com/amazon/ask/helloworld/handlers/FallbackIntentHandler.java
@@ -0,0 +1,31 @@
+package com.amazon.ask.helloworld.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+// 2018-July-09: AMAZON.FallackIntent is only currently available in en-US locale.
+//              This handler will not be triggered except in that locale, so it can be
+//              safely deployed for any locale.
+public class FallbackIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("AMAZON.FallbackIntent"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "Sorry, I don't know that. You can say try saying help!";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+
+}
