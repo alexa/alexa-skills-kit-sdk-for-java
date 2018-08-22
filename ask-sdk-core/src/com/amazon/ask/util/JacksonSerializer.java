98450a24d8caa98e152a39b5b5b10be21747db53
Jonathan Breedlove <breedloj@amazon.com>
2018-08-22 16:01:03 -0700

    Serialize dates to ISO 8601 strings

--- ask-sdk-core/src/com/amazon/ask/util/JacksonSerializer.java
+++ ask-sdk-core/src/com/amazon/ask/util/JacksonSerializer.java
@@ -18,6 +18,7 @@ import com.amazon.ask.exception.AskSdkException;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
@@ -33,6 +34,7 @@ public final class JacksonSerializer implements Serializer {
     static {
         mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         mapper.registerModule(new JavaTimeModule());
     }
 
