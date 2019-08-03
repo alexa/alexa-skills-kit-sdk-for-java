f7948219cf02f50d299186591e53144a28f04c7b
Pati <sattwikp@38f9d3570358.ant.amazon.com>
2019-08-02 20:16:08 -0700

    Basic working changes

--- samples/helloworld/src/com/amazon/ask/helloworld/LocalDebugger.java
+++ samples/helloworld/src/com/amazon/ask/helloworld/LocalDebugger.java
@@ -1,24 +1,106 @@
 package com.amazon.ask.helloworld;
-import java.io.IOException;
 
-import static spark.Spark.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
 public class LocalDebugger {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        port(8080);
-        post("/", (req, res) -> HelloWorldStreamHandler.getSkill());
-        boolean isWindows = System.getProperty("os.name")
-  .toLowerCase().startsWith("windows");
-        String homeDirectory = System.getProperty("user.home");
-        Process process;
-        if (isWindows) {
-            process = Runtime.getRuntime()
-            .exec(String.format("cmd.exe /c ngrok http 8080", homeDirectory));
+    private static final String PORT_NUMBER = "portNumber";
+    private static final String TIME_OUT = "timeOut";
+    private static int timeOut = 10000;
+    private static int port;
+
+    public static void main(String[] args) throws InterruptedException, IOException, SecurityException, ParseException {
+        HelloWorldStreamHandler skillInvoker = new HelloWorldStreamHandler();
+        CommandLine cmd = ParseCommandLineArgs(args);
+        if (cmd.getOptionValue(PORT_NUMBER) == null || cmd.getOptionValue(PORT_NUMBER).isEmpty()) {
         } else {
-            process = Runtime.getRuntime()
-            .exec(String.format("sh -c ngrok http 8080", homeDirectory));
+            port = Integer.parseInt(cmd.getOptionValue(PORT_NUMBER));
         }
-        int exitCode = process.waitFor();
-        assert exitCode == 0;
+
+        if (cmd.getOptionValue(TIME_OUT) == null || cmd.getOptionValue(TIME_OUT).isEmpty()) {
+        } else {
+            timeOut = Integer.parseInt(cmd.getOptionValue(TIME_OUT));
+        }
+
+        ServerSocket serverSocket = CreateDebuggerSocket(port, timeOut);
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String payload = extractPayload(br);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                skillInvoker.handleRequest(IOUtils.toInputStream(payload.toString(), "UTF-8"), baos, null);
+                JSONObject jsonResponse = new JSONObject(baos.toString("UTF-8"));
+                out.write("HTTP/1.1 200 OK\r\n" + "Content-Type: application/json;charset=UTF-8\r\n" + "Content-Length: "
+                        + jsonResponse.toString().length() + "\r\n\r\n" + jsonResponse.toString());
+                out.flush();
+                out.close();
+            }
+        } catch (SocketTimeoutException e) {
+            serverSocket.close();
+        }
+    }
+
+    private static CommandLine ParseCommandLineArgs(String[] args) throws ParseException {
+        Options options = new Options();
+
+        Option portNumber = new Option("p", PORT_NUMBER, true, "port number for local debuging session");
+        portNumber.setRequired(true);
+        options.addOption(portNumber);
+
+        Option debugTimeout = new Option("t", TIME_OUT, true, "time in milliseconds for which the socket connection is allowed to be inactive");
+        debugTimeout.setRequired(false);
+        options.addOption(debugTimeout);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp(Thread.currentThread().getStackTrace()[1].getClassName(), options);
+            throw e;
+        }
+        return cmd;
+    }
+
+    private static ServerSocket CreateDebuggerSocket(int portNumber, int socketTimeOut) throws IOException, SocketException {
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        serverSocket.setReuseAddress(true);
+        serverSocket.setSoTimeout(socketTimeOut);
+        return serverSocket;
+    }
+
+    private static String extractPayload(BufferedReader br) throws IOException {
+        while ((br.readLine()).length() != 0) {
+        }
+
+        // code to read the post payload data
+        StringBuilder payload = new StringBuilder();
+        while (br.ready()) {
+            payload.append((char) br.read());
+        }
+        return payload.toString();
     }
 }
