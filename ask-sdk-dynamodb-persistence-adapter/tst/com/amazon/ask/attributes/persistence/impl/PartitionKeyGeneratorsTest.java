f5ed03eba52bf6cfa5c86950e5164dda66b3e343
Jonathan Breedlove <breedloj@amazon.com>
2019-11-13 14:20:32 -0800

    Add Person ID partition key generator

--- ask-sdk-dynamodb-persistence-adapter/tst/com/amazon/ask/attributes/persistence/impl/PartitionKeyGeneratorsTest.java
+++ ask-sdk-dynamodb-persistence-adapter/tst/com/amazon/ask/attributes/persistence/impl/PartitionKeyGeneratorsTest.java
@@ -16,6 +16,7 @@ package com.amazon.ask.attributes.persistence.impl;
 import com.amazon.ask.exception.PersistenceException;
 import com.amazon.ask.model.Context;
 import com.amazon.ask.model.Device;
+import com.amazon.ask.model.Person;
 import com.amazon.ask.model.RequestEnvelope;
 import com.amazon.ask.model.User;
 import com.amazon.ask.model.interfaces.system.SystemState;
@@ -119,5 +120,81 @@ public class PartitionKeyGeneratorsTest {
         RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
         PartitionKeyGenerators.deviceId().apply(env);
     }
+
+    @Test
+    public void person_id_generator_persistence_id_generated_from_person_id() {
+        String personId = "1234";
+
+        Person person = Person.builder().withPersonId(personId).build();
+        SystemState system = SystemState.builder().withPerson(person).build();
+        Context context = Context.builder().withSystem(system).build();
+        RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
+        Assert.assertEquals(PartitionKeyGenerators.personId().apply(env), personId);
+    }
+
+    @Test
+    public void person_id_generator_persistence_id_generated_from_user_id_person_id_null() {
+        String userId = "1234";
+
+        User user = User.builder().withUserId(userId).build();
+        Person person = Person.builder().withPersonId(null).build();
+        SystemState system = SystemState.builder().withPerson(person).withUser(user).build();
+        Context context = Context.builder().withSystem(system).build();
+        RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
+
+        Assert.assertEquals(PartitionKeyGenerators.personId().apply(env), userId);
+    }
+
+    @Test
+    public void person_id_generator_persistence_id_generated_from_user_id_person_null() {
+        String userId = "1234";
+
+        User user = User.builder().withUserId(userId).build();
+        SystemState system = SystemState.builder().withPerson(null).withUser(user).build();
+        Context context = Context.builder().withSystem(system).build();
+        RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
+
+        Assert.assertEquals(PartitionKeyGenerators.personId().apply(env), userId);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void person_id_generator_persistence_id_person_id_user_id_null_throws_exception() {
+        User user = User.builder().withUserId(null).build();
+        Person person = Person.builder().withPersonId(null).build();
+        SystemState system = SystemState.builder().withPerson(person).withUser(user).build();
+        Context context = Context.builder().withSystem(system).build();
+        RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
+
+        PartitionKeyGenerators.personId().apply(env);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void person_id_generator_persistence_id_person_id_user_null_throws_exception() {
+        Person person = Person.builder().withPersonId(null).build();
+        SystemState system = SystemState.builder().withPerson(person).withUser(null).build();
+        Context context = Context.builder().withSystem(system).build();
+        RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
+
+        PartitionKeyGenerators.personId().apply(env);
+    }
+
+    @Test (expected = PersistenceException.class)
+    public void person_id_generator_envelope_null_throws_exception() {
+        PartitionKeyGenerators.personId().apply(null);
+    }
+
+    @Test (expected = PersistenceException.class)
+    public void person_id_generator_context_null_throws_exception() {
+        RequestEnvelope env = RequestEnvelope.builder().build();
+        PartitionKeyGenerators.personId().apply(env);
+    }
+
+    @Test (expected = PersistenceException.class)
+    public void person_id_generator_systemState_null_throws_exception() {
+        Context context = Context.builder().build();
+        RequestEnvelope env = RequestEnvelope.builder().withContext(context).build();
+        PartitionKeyGenerators.personId().apply(env);
+    }
+
 
 }
