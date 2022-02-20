d0b921516531d94171f42ef047b29d6fe76d0ffd
gbatt <gbatt@amazon.com>
2022-02-20 18:10:13 +0000

    Bumps in jvm, akexa sdk, log4j

--- samples/helloworld-kt/build.gradle.kts
+++ samples/helloworld-kt/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
@@ -13,8 +13,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.amazon.alexa:ask-sdk:2.40.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.8.2")
+    implementation("com.amazon.alexa:ask-sdk:2.42.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
 }
 
 
